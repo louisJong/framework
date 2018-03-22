6a004ab07b44390c2b3c3fcc6463bd63c47c7ac3
louisJong <237627805@qq.com>
2018-03-22 16:06:49 +0800

    mysql主从分离切面配置

--- framework-dao/src/main/java/com/song/framework/dao/base/ProtoMapper.java
+++ framework-dao/src/main/java/com/song/framework/dao/base/ProtoMapper.java
@@ -3,6 +3,9 @@ package com.song.framework.dao.base;
 import java.util.List;
 import java.util.Map;
 
+import com.song.framework.dao.base.routing.RoutingDataSource;
+import com.song.framework.dao.base.routing.RoutingStrategy;
+
 public interface ProtoMapper< T extends ProtoBean > {
 
     /**
@@ -11,6 +14,7 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @param bean 需持久化的数据对象
      * @throws Exception 数据库访问异常
      */
+	@RoutingDataSource(RoutingStrategy.WRITE)
     int insert( T bean ) throws Exception;
     
     /**
@@ -19,6 +23,7 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @return int
      * @throws Exception
      */
+	@RoutingDataSource(RoutingStrategy.WRITE)
     int batchInsert(List<T> list) throws Exception;
 
     /**
@@ -27,6 +32,7 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @param id 要删除记录的主键
      * @throws Exception 数据库访问异常
      */
+	@RoutingDataSource(RoutingStrategy.WRITE)
     int delete( Integer id ) throws Exception;
 
     /**
@@ -35,6 +41,7 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @param bean 需持久化的数据对象
      * @throws Exception 数据库访问异常
      */
+	@RoutingDataSource(RoutingStrategy.WRITE)
     int update( T bean ) throws Exception;
     
     /**
@@ -43,6 +50,7 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @return
      * @throws Exception
      */
+	@RoutingDataSource(RoutingStrategy.WRITE)
     int updateByVersion(T bean) throws Exception;
 
     /**
@@ -51,6 +59,7 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @param id 主键
      * @throws Exception 数据库访问异常
      */
+	@RoutingDataSource(RoutingStrategy.READ)
     T selectById( Object id ) ;
 
     /**
@@ -59,6 +68,7 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @param map 查询条件映射
      * @throws Exception 数据库访问异常
      */
+	@RoutingDataSource(RoutingStrategy.READ)
     Integer selectCount( Map< String, Object > map ) ;
 
     /**
@@ -67,5 +77,6 @@ public interface ProtoMapper< T extends ProtoBean > {
      * @param map 查询条件映射
      * @throws Exception 数据库访问异常
      */
+	@RoutingDataSource(RoutingStrategy.READ)
     List< T > selectList( Map< String, Object > map ) ;
 }
